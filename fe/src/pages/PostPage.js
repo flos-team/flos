f926ad110a47dda581f4d0f95074ed8f0291ee14
김주하 <zouamare@gmail.com>
2023-01-26 11:04:07 +0900

    feat : 라우팅 기능 추가

---
+++ fe/src/pages/PostPage.js
@@ -0,0 +1,12 @@
+import {Nav} from "../components/BottomNavigation"
+
+function Post() {
+    return (
+        <div>
+            Post Page
+            <Nav></Nav>
+        </div>
+    );
+}
+
+export default Post;
---
+++ fe/src/pages/ProfilePage.js
@@ -0,0 +1,12 @@
+import {Nav} from "../components/BottomNavigation"
+
+function Profile() {
+    return (
+        <div>
+            Profile Page
+            <Nav></Nav>
+        </div>
+    );
+}
+
+export default Profile;
