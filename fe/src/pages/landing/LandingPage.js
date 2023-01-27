4227a144cfe81ad43ac81fdd5dbd5278f95e034f
김지환 <tykimdream@naver.com>
2023-01-27 09:39:12 +0900

    feat : 랜딩페이지 구현 및 점멸 기능

--- fe/src/pages/landing/LandingPage.js
+++ fe/src/pages/landing/LandingPage.js
@@ -1,19 +1,23 @@
 import styles from "./LandingPage.module.css";
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 function Landing() {
-    return (
-        <div className={styles.landingRoot}> 
-            Landing Page
-            <Link to = "/home">
-                <div>home</div>
-            </Link>
-            <Link to = "/login">
-                <div>login</div>
-            </Link>
-        </div>
-    );
+  return (
+    <div className={styles.landingRoot}>
+      <div className={styles.groomsSmall2}></div>
+      <div className={styles.mainLogo}>
+        <div className={styles.title}>Flos</div>
+        <div className={styles.subTitle}>당신의 마음이 꽃 피는 곳</div>
+      </div>
+      <div className={styles.guideText}>
+        <Link to="/home">
+          <div className={`${styles.startBtn} ${styles.blinking}`}>탭하여 시작하기</div>
+        </Link>
+      </div>
+      <div className={styles.spacer}></div>
+    </div>
+  );
 }
 
 export default Landing;
