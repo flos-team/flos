ae72f7b5abad82da3bb90ec07f51218a4e005042
조원희 <onehee9710@gmail.com>
2023-01-27 17:02:09 +0900

    file : 프로필 컴포넌트(페이지)  및 헤더 파일 생성

---
+++ fe/src/pages/main/ProfilePage.js
@@ -0,0 +1,12 @@
+import { render } from "@testing-library/react";
+import React from "react";
+
+const ProfilePage = () => {
+    return (
+        <>
+        <h1>hey</h1>
+        </>
+      );
+}
+
+export default ProfilePage;
