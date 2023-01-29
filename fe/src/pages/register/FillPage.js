4f78cfd5b8fcca2c87219dbb055d59719c903f76
baamkyu <baamkyu@gmail.com>
2023-01-29 22:24:11 +0900

    feat: 중복확인 조건부 스타일링 추가

--- fe/src/pages/register/FillPage.js
+++ fe/src/pages/register/FillPage.js
@@ -3,105 +3,99 @@ import styles from "./FillPage.module.css";
 
 
 function FillPage() {
-  const [inputId, setInputId] = useState('')
-  const [inputPw, setInputPw] = useState('')
-  const [inputCheckPw, setInputCheckPw] = useState('')
-  const [inputNickname, setInputNickname] = useState('')
-  const [emailMsg, setEmailMsg] = useState('중복확인이 필요한 항목입니다.')
-  const [pwMsg1, setPwMsg1] = useState('')
-  const [pwMsg2, setPwMsg2] = useState('')
-  const [nicknameMsg1, setNicknameMsg1] = useState(false)
-  const [nicknameMsg2, setNicknameMsg2] = useState(false)
+  const [inputValue, setInputValue] = useState({
+    inputId: '',
+    inputPw: '',
+    inputCheckPw: '', 
+    inputNickname: '',
+  })
+
+  const { inputId, inputPw, inputCheckPw, inputNickname } = inputValue;
+
+  // Input tag handling
+  const handleInput = (e) => {
+    const { name, value } = e.target
+    setInputValue({
+      ...inputValue,
+      [name]: value
+    })
+  }
+
+  const [emailMsg, setEmailMsg] = useState(false)
+  const [pwMsg, setPwMsg] = useState('')
+  const [pwCheckMsg, setPwCheckMsg] = useState('')
+  const [nicknameMsg, setNicknameMsg] = useState(false)
+
+  const [emailCheck, setEmailCheck] = useState(false)
+  const [nicknameCheck, setNicknameCheck] = useState(false)
 
   // 이메일 - 문자열에 특수문자, 영어, 숫자만 있는지 확인
   // 비밀번호 - 문자열에 특수문자, 영어, 숫자만 있는지 확인
   // 닉네임 - 문자열에 한글, 영어, 숫자만 있는지 확인
 
-  let check_num = /[0-9]/; // 숫자 
-  let check_eng = /[a-zA-Z]/; // 문자 
-  let check_spc = /[~!@#$%^&*()_+|<>?:{}]/; // 특수문자
-  let check_kor = /[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/; // 한글
+  const check_num = /[0-9]/; // 숫자 
+  const check_eng = /[a-zA-Z]/; // 문자 
+  const check_spc = /[~!@#$%^&*()_+|<>?:{}]/; // 특수문자
+  const check_pw = /^[a-z|A-Z|0-9|~!@#$%^&*()_+|<>?:{}]+$/ // 숫자, 문자, 특수문자만 사용 가능
+  const check_nickname = /^[ㄱ-ㅎ|가-힣|a-z|A-Z|0-9|]+$/ // 한글, 영어, 숫자만 사용 가능
 
-  // ⭐️⭐️⭐️ID 사용자 검증 로직⭐️⭐️⭐️
-  const handleInputId = (e) => {
-    setInputId(e.target.value)
-  }
+  // 1. ID 유효성 검사
+  const isValidId = inputId.includes('@') && inputId.includes('.')
 
-  // 중복확인 눌렀을 때 작동하는 함수
-  const checkIdOverlap = (e) => {
-    console.log(inputId)
-    if (!inputId.includes('@') || !inputId.includes('.') || check_kor.test(inputId)){
+  // 1. PW 유효성 검사
+  const isValidPwLength = inputPw.length>=8 && inputPw.length<=12
+  const isValidPwForm = check_pw.test(inputPw) && check_num.test(inputPw) && check_eng.test(inputPw) && check_spc.test(inputPw)
+
+  // 1. 닉네임 유효성 검사
+  const isValidNicknameLength = inputNickname.length>=2 && inputNickname.length<=10
+  const isValidNicknameForm = check_nickname.test(inputNickname)
+
+
+  // 2. ID 유효성 검사 로직
+  const checkId = () => {
+    if (isValidId === false){
       setEmailMsg('잘못된 유형의 이메일 주소입니다.')
     }
-    // 이미 백엔드 데이터에 존재하는 이메일 주소일 때 => 이미 가입된 이메일입니다.
-    // else if (이미 가입된 이메일이면) {
-    //   setEmailMsg('이미 가입된 이메일입니다.')
-    // }
     else {
       setEmailMsg('사용 가능한 이메일입니다.')
-      setInputId(inputId)
     }
   }
 
-  // ⭐️⭐️⭐️비밀번호 사용자 검증 로직⭐️⭐️⭐️
-  const handleInputPw = (e) => {
-    setInputPw(e.target.value)
-    console.log(e.target.value)
-    // 비밀번호 길이 검증 (8~12자))
-    if (inputPw.length>=8 && inputPw.length<=12){
-      setPwMsg1('길이통과')
+  // 2. PW 유효성 검사 로직
+  const checkPw = () => {
+    if (isValidPwLength && isValidPwForm){
+      setPwMsg('사용 가능한 비밀번호입니다')
     } else {
-      setPwMsg1('길이오류')
-    }
-    // 비밀번호 양식 검증 (영문, 숫자, 특수문자 모두 포함)
-    if (check_eng.test(inputPw) && check_num.test(inputPw) && check_spc.test(inputPw)) {
-      setPwMsg2('다 포함함')
-    } else {
-      setPwMsg2('다 포함해야함')
+      setPwMsg('사용할 수 없는 비밀번호입니다')
     }
   }
-  
-  // 비밀번호 = 비밀번호 확인 검사
-  const handleInputCheckPw = (e) => {
-    setInputCheckPw(e.target.value)
-    console.log(e.target.value)
+
+  // 2. 비밀번호확인 유효성 검사 로직
+  const reCheckPw = () => {
     if (inputCheckPw === inputPw) {
-      console.log('동일함')
+      setPwCheckMsg('비밀번호 확인 완료')
     }
   }
 
-  // ⭐️⭐️⭐️닉네임 사용자 검증 로직⭐️⭐️⭐️
-  const handleNickname = (e) => {
-    setInputNickname(e.target.value)
-    if (inputNickname.length>=2 && inputNickname.length<=10) {
-      setNicknameMsg1(true)
+  // 2. 닉네임 유효성 검사 로직
+  const checkNickname = () => {
+    if (isValidNicknameLength && isValidNicknameForm){
+      setNicknameMsg('사용 가능한 닉네임입니다')
     } else {
-      setNicknameMsg1(false)
-    }
-    if (check_kor.test(inputNickname) || check_num.test(inputNickname) || check_eng.test(inputNickname)) {
-      setNicknameMsg2(true)
-    } else {
-      setNicknameMsg2(false)
+      setNicknameMsg('사용할 수 없는 닉네임입니다')
     }
   }
-  const handleInputNickname = (e) => {
-    setInputNickname(e.target.value)
+
+
+  // 3. 아이디 중복확인
+  const checkEmailOverlap = () => {
+    setEmailCheck(true)
   }
 
-
-  // const checkPwOverlap = () => {
-  //   if (inputPw.length<8 || inputPw.length>12){
-  //     setPwMsg1('길이오류')
-  //   }
-  //   else {
-  //     setPwMsg1('길이통과')
-  //   }
-  // }
-
-  // const checkNicknameOverlap = () => {
-  //   console.log(inputNickname)
-  // }
-  
+  // 3.닉네임 중복확인
+  const checkNicknameOverlap = () => {
+    setNicknameCheck(true)
+  }
 
 
   return (
@@ -125,30 +119,31 @@ function FillPage() {
       <div>
         <div>
           <p>이메일(아이디)</p>
-          <input type='text' name='input_id' placeholder='flos@example.com'
-            value={ inputId } onChange={ handleInputId } className={styles.inputdiv}/>
-          <button onClick={ checkIdOverlap }>중복확인</button>
+          <input type='text' name='inputId' placeholder='flos@example.com'
+            onChange={ handleInput } onKeyUp={ checkId } className={styles.inputdiv}/>
+          <button onClick= { checkEmailOverlap }>중복확인</button>
           <p>{emailMsg}</p>
+          <p className={ emailCheck ? styles.canuse : styles.cannotuse }>중복확인</p>
         </div>
         <div>
           <p>비밀번호</p> 
-          <input type='password' name='input_pw' placeholder='영문, 숫자, 특수문자 8~12자'
-          value={ inputPw } onChange={ handleInputPw } className={styles.inputdiv} />
-          <p>{pwMsg1}</p>
-          <p>{pwMsg2}</p>
+          <input type='password' name='inputPw' placeholder='영문, 숫자, 특수문자 8~12자'
+          onChange={ handleInput } onKeyUp={ checkPw } className={styles.inputdiv} />
+          <p>{pwMsg}</p>
         </div>
         <div>
           <p>비밀번호 확인</p>
-          <input type='password' name='input_checkpw' placeholder='비밀번호 재입력'
-          value={ inputCheckPw } onChange={ handleInputCheckPw } className={styles.inputdiv} />
+          <input type='password' name='inputCheckPw' placeholder='비밀번호 재입력'
+          onChange={ handleInput } onKeyUp={ reCheckPw } className={styles.inputdiv} />
+          <p>{ pwCheckMsg }</p>
         </div>
         <div>
           <p>닉네임</p>
-          <input type='text' name='input_nickname' placeholder='한글, 영문, 숫자 2~10자'
-          value={ inputNickname } onChange={ handleNickname } className={styles.inputdiv} />
-          <p>{ nicknameMsg1 }</p>
-          <p>{ nicknameMsg2 }</p>
-          {/* <button onClick={ checkNicknameOverlap }>중복확인</button> */}
+          <input type='text' name='inputNickname' placeholder='한글, 영문, 숫자 2~10자'
+          onChange={ handleInput } onKeyUp={ checkNickname }className={styles.inputdiv} />
+          <button onClick={ checkNicknameOverlap }>중복확인</button>
+          <p>{ nicknameMsg }</p>
+          <span className={ nicknameCheck ? styles.canuse : styles.cannotuse }>중복확인</span>
         </div>
       </div>
     </div>
