2b58cee5a6ae1f186bd1fa0457f80d68d0321092
1-Hee <onehee9710@gmail.com>
2023-01-29 15:27:57 +0900

    file: assets 폴더에 ProfileAsset 폴더 생성 및 이미지 추가 / main 폴더 밑에 profilePage.scss 생성 및 css 파일 추가

--- fe/src/pages/main/ProfilePage.js
+++ fe/src/pages/main/ProfilePage.js
@@ -2,11 +2,15 @@ import { render } from "@testing-library/react";
 import React from "react";
 
 const ProfilePage = () => {
-    return (
-        <>
-        <h1>hey</h1>
-        </>
-      );
-}
+  return (
+    <>
+      <div className="user-info-header">
+        <img className="user-profile-img"></img>
+        <p className="user-nickname">wonny</p>
+        <div className="user-today-mood">무드</div>
+      </div>
+    </>
+  );
+};
 
 export default ProfilePage;
