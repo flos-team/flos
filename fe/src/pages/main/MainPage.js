76867c7ece2abbac011b7f7af52b2a7508cc2d8b
1-Hee <onehee9710@gmail.com>
2023-01-29 22:35:09 +0900

    feat : components 디렉토리 밑에 HeaderComponent 작성 완료 및 props에 따른 페이지별 확장성 추가 완료, file : ProfilePage 컴포넌트의 Local Header 제거

--- fe/src/pages/main/MainPage.js
+++ fe/src/pages/main/MainPage.js
@@ -1,35 +1,37 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 import Nav from "../../components/BottomNavigation";
 import Feed from "./FeedPage";
 /* 프로필 페이지 import */
 import ProfilePage from "./ProfilePage";
 
 function Main() {
-    const [position, setPosition] = useState('feed')
+  const [position, setPosition] = useState("feed");
 
-    const getPosition = (position) =>{
-        setPosition(position);
-    }
-    let currentPage;
-    if(position==='feed'){
-        currentPage = <Feed />
-    } else if(position==='global'){
-        currentPage = <div>global</div>
-    } else if(position==='home'){
-        currentPage = <div>home</div>
-    } else if(position==='garden'){
-        currentPage = <div>garden</div>
-    } else if(position==='profile'){
-        currentPage = <ProfilePage></ProfilePage>
-    } else{
-        currentPage = <div>404</div>
-    }
-    return(
-        <div>
-           {currentPage} 
-            <Nav position = {position} getPosition = {getPosition}></Nav>
-        </div>
-    )
+  const getPosition = (position) => {
+    setPosition(position);
+  };
+  let currentPage;
+  if (position === "feed") {
+    currentPage = <Feed />;
+  } else if (position === "global") {
+    currentPage = <div>global</div>;
+  } else if (position === "home") {
+    currentPage = <div>home</div>;
+  } else if (position === "garden") {
+    currentPage = <div>garden</div>;
+  } else if (position === "profile") {
+    currentPage = <ProfilePage></ProfilePage>;
+  } else if (position === "test") {
+    currentPage = <div>test</div>;
+  } else {
+    currentPage = <div>404</div>;
+  }
+  return (
+    <div>
+      {currentPage}
+      <Nav position={position} getPosition={getPosition}></Nav>
+    </div>
+  );
 }
 
 export default Main;
