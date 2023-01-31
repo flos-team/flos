ba9d283ba5307682b6995ae2f5e5c44afdaa5517
joykst96 <joykst96@gmail.com>
2023-01-31 09:45:04 +0900

    feat: 파일 업로드/다운로드 추가

--- be/src/main/java/com/onehee/flos/model/service/MemberService.java
+++ be/src/main/java/com/onehee/flos/model/service/MemberService.java
@@ -3,12 +3,22 @@ package com.onehee.flos.model.service;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.onehee.flos.auth.model.dto.TokenResponse;
 import com.onehee.flos.model.dto.request.LoginRequestDTO;
-import com.onehee.flos.model.dto.request.SignUpRequestDTO;
-import com.onehee.flos.model.dto.response.MemberResponseDTO;
+import com.onehee.flos.model.dto.request.MemberFindPasswordDTO;
+import com.onehee.flos.model.dto.request.MemberSignUpRequestDTO;
+import com.onehee.flos.model.dto.request.MemberUpdateRequestDTO;
 import org.springframework.stereotype.Service;
 
 @Service
 public interface MemberService {
-    void signUp(SignUpRequestDTO signUpRequestDTO);
+
+    void createMember(MemberSignUpRequestDTO memberSignUpRequestDTO);
+
     TokenResponse login(LoginRequestDTO loginRequestDTO) throws JsonProcessingException;
+
+    void updateMember(MemberUpdateRequestDTO memberUpdateRequestDTO);
+
+    void deleteMember();
+
+    void resetPassword(MemberFindPasswordDTO memberFindPasswordDTO);
+
 }
