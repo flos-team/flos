1ea1d7bc283c8944f4f4e950bd0a03802cacb003
조원희 <onehee9710@gmail.com>
2023-02-01 11:18:29 +0900

    design: HeaderCompoent의 UI를 머터리얼 디자인을 준수하여 재구성

--- fe/src/components/HeaderComponent.js
+++ fe/src/components/HeaderComponent.js
@@ -1,66 +1,73 @@
 import { useState } from "react";
 
 /* import image assets */
-import angleLeft from "../assets/GlobalAsset/fi-br-angle-left.png";
-import notification from "../assets/GlobalAsset/notification.png";
-import settingBtn from "../assets/ProfileAsset/setting-btn.png";
+import arrowLeft from "../assets/GlobalAsset/arrow-left.png";
+import notifyIcon from "../assets/GlobalAsset/notification.png";
+import settingIcon from "../assets/ProfileAsset/setting-btn.png";
+import checkIcon from "../assets/GlobalAsset/check-icon.png";
 import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
 
 /* import css */
 import "./HeaderComponent.css";
-/** backVisible((bool)true, false) : 뒤로 가기 표시, pageName((string)현재 페이지 이름) : 현재 페이지 이름, optType((int) 0 : noti Icon, 1 : setting Icon)
+/** 
+ * backVisible:Boolean/뒤로 가기 표시, pageName:String/현재 페이지 이름, optType:Integer/{0:알림, 1:세팅, 2:체크}
  */
 const HeaderComponent = ({ backVisible, pageName, optType }) => {
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
 
-  /* 뒤로갈 페이지가 있을 경우 arrow 표시 */
-  const backImg = backVisible ? (
-    <img src={angleLeft} onClick={() => navigate(-1)}></img>
-  ) : (
-    <></>
-  );
-  const notificationCase = (
-    <Link to="/notification">
-      <div className="btn-div">
-        <img className="option-btn" src={notification}></img>
-      </div>
-    </Link>
-  );
+  // /* 뒤로갈 페이지가 있을 경우 arrow 표시 */
+  // const backImg = backVisible ? (
+  //   <img src={angleLeft} onClick={() => navigate(-1)}></img>
+  // ) : (
+  //   <></>
+  // );
+  // const notificationCase = (
+  //   <Link to="/notification">
+  //     <div className="btn-div">
+  //       <img className="option-btn" src={notification}></img>
+  //     </div>
+  //   </Link>
+  // );
 
-  const settingCase = (
-    <Link to="/settings">
-      <div className="btn-div">
-        <img className="option-btn" src={settingBtn}></img>
-      </div>
-    </Link>
-  );
-  /* props로 들어오는 optType(int) 에 따라 상단 옵션 버튼이 달라짐 */
-  // let imgSrc = null;
-  let btnSelected = (<></>);
+  let optBTn = null;
   switch (optType) {
-    case 0:
-      btnSelected = notificationCase;
+    case 0: // 알림
+      optBTn = <img className="opt-icon" src={notifyIcon}></img>;
       break;
-    case 1:
-      btnSelected = settingCase;
+    case 1: // 세팅
+      optBTn = <img className="opt-icon" src={settingIcon}></img>;      
+      break;
+    case 2: // 체크
+      optBTn = <img className="opt-icon" src={checkIcon}></img>;      
       break;
     default:
-      btnSelected = '';
+      optBTn = <></>
       break;
   }
+
+  let arrowBtn = backVisible?<div className="back-btn"><img className="back-icon" src={arrowLeft} /></div>:<></>
+
+
+  // const settingCase = (
+  //   <Link to="/settings">
+  //     <div className="btn-div">
+  //       <img className="option-btn" src={settingBtn}></img>
+  //     </div>
+  //   </Link>
+  // );
 
   
   return (
     <>
-      <div className="page-header">
-        <div className="page-div">
-          <div className="title-bar">
-            {backImg}
-            <p>{pageName}</p>
-          </div>
-          {btnSelected}
-        </div>          
+      <div className="top-bar">
+        {arrowBtn}
+        <div className="title-div">
+          <p className="title-name">{pageName}</p>
+        </div>
+        <div className="opt-btn">
+          {optBTn}
+        </div>
       </div>
     </>
   );
