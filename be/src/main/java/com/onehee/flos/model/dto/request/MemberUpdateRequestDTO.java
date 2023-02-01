f901ea40761bf8fc9d921b5428e889c7300e84dc
joykst96 <joykst96@gmail.com>
2023-02-01 16:03:27 +0900

    fix: CORS에 localhost:3000 과 server:80 설정

--- be/src/main/java/com/onehee/flos/model/dto/request/MemberUpdateRequestDTO.java
+++ be/src/main/java/com/onehee/flos/model/dto/request/MemberUpdateRequestDTO.java
@@ -2,11 +2,13 @@ package com.onehee.flos.model.dto.request;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 import org.springframework.web.multipart.MultipartFile;
 
 @Getter
+@Setter
 @RequiredArgsConstructor
 public class MemberUpdateRequestDTO {
-    private String nickname;
-    private MultipartFile profileImage;
+    private final String nickname;
+    private final MultipartFile profileImage;
 }
