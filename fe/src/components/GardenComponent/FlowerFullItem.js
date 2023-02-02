a82b2e462034da2936da0a6b6c3b5d8fe2408bac
김주하 <zouamare@gmail.com>
2023-02-02 13:20:42 +0900

    feat: 가든 페이지 분할 및 배치

---
+++ fe/src/components/GardenComponent/FlowerFullItem.js
@@ -0,0 +1,47 @@
+import styled from "@emotion/styled";
+import GardenFullComponentDetailData from "./GardenFullComponentDetailData"
+
+const FlowerView = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
+
+const FlowerDetailData = styled.div`
+    flex-grow: 1;
+`;
+
+const FullItem = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`;
+
+const FlowerFullItem = (props) => {
+    let flowerInfo = props.flowerInfo;
+    let flowerImgData = {
+        flowerColor: "red"
+    }
+    let flowerData = {
+        state: "은은한",
+        name: "춘식",
+        length: 15,
+        startDate: "2022-10-11",
+        endDate: "2022-10-30"
+    }
+    return (
+        <FullItem>
+            <FlowerView>
+                이미지
+                이미지
+                이미지
+            </FlowerView>
+            <FlowerDetailData>
+                <GardenFullComponentDetailData flowerData={flowerData} />
+            </FlowerDetailData>
+        </FullItem>
+    );
+}
+
+export default FlowerFullItem;
