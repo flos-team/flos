b482eb9bc53a0f371937aaecf09718576d64e6b5
김주하 <zouamare@gmail.com>
2023-02-03 17:49:58 +0900

    feat: 홈페이지 progressbar 연동 및  element 차감 기능 구현

--- fe/src/pages/main/HomePage.js
+++ fe/src/pages/main/HomePage.js
@@ -14,29 +14,6 @@ import rainAnimationData from "../../assets/HomeAsset/81756-rain.json"
 import sunObjectData from "../../assets/HomeAsset/22190-sunny-day.json"
 import Lottie from 'react-lottie';
 
-let flower_state = true;
-
-const floating = keyframes`
-    0 {
-        transform: translateY(0);    
-    }
-    50% {
-        transform: translateY(-15px);
-    }
-    100% {
-        transform: translateY(0);
-    }
-`
-
-const boxStyle = css`
-    width: 50px; 
-    height: 50px; 
-    border-radius: 100%;
-    background: #a951bf;
-    animation: ${floating} 2s ease infinite;
-`;
-
-
 const Title = styled.h1`
   color: #007bff;
   font-size: 32px;
@@ -52,20 +29,53 @@ const WeatherAnimation = styled.div`
     hegiht: 100%;
 `;
 
+const makeNewSeed = () => {
+
+}
+
+const flowering = () => {
+    console.log("개화 시작");
+    return (
+        <>
+            <button>개화</button>
+        </>
+    );
+}
+
 const Home = () => {
+    const [flowerInfo, setFlowerInfo] = useState({
+        existence: "",    // 존재 여부
+        name: "", // 이름
+        sunElementCount: 0, 
+        rainElementCount: 0,
+        CurrentGrowthValue: 0,
+        MaxGrowthValue: 100
+    });
 
     useEffect(() => {
-        if (flower_state === true) {    // 여기에서 꽃 상태 받아옴 (normal / fullfilled)
-            flower_state = false;
-            setTimeout(() => { console.log("나와"); flower_state = true }, 2000);
+        // --------------------- 꽃 상태 받아옴 (empty/ normal / fullfilled) 시작 ---------------------
+
+        setFlowerInfo({
+            existence: "fulfilled",    // 존재 여부
+            name: "명식이", // 이름
+            sunElementCount: 20,    // 태양 요소 개수
+            rainElementCount: 20,   // 빗물 요소 개수
+            CurrentGrowthValue: 60, // 현재 고정 값
+            MaxGrowthValue: 100 // 최대 값
+        });   // flower 상태 일반 상태로 부여
+
+        // --------------------- 꽃 상태 받아옴 (empty/ normal / fullfilled) 끝 ---------------------
+
+        /*
+            꽃 정보에 따른 추가 작업
+        */
+        if (flowerInfo.flowerExistence === "empty") {  // 현재 키우고 있는 꽃이 없을 경우
+            makeNewSeed();  // 새로운 꽃을 만든다.
         }
-        console.log('컴포넌트가 화면에 나타남')
+        else if(flowerInfo.flowerExistence === "fulfilled") {  // 현재 키우고 있는 꽃이 있고 게이지가 가득 찬 경우
+            flowering();
+        }
 
-        // 각 상태에 맞는 동작
-
-        // return () => {
-        //     console.log('컴포넌트가 화면에서 사라짐');
-        // };
     }, []);
 
     const sunClick = () => {    // 해 버튼을 클릭 했을 경우,
@@ -76,6 +86,8 @@ const Home = () => {
             autoplay: true,
             animationData: sunObjectData,
         }
+        // 서버랑 통신해서 sun이 소진되었다고 전송
+        flowerInfo.sunElementCount--;
         setSunAnimation(<>
             <div ref={sunBox}>
                 <Lottie options={sunObjectOptions} height={1000} width={1000} />
@@ -93,17 +105,18 @@ const Home = () => {
             autoplay: true,
             animationData: rainAnimationData,
         }
-
+        // 서버랑 통신해서 rain이 소진되었다고 전송
+        flowerInfo.rainElementCount--;
         const rainObjectOptions = {
             autoplay: true,
             animationData: rainObjectData,
         }
         setRainAnimation(
         <WeatherAnimation>
-            <div ref={rainBox} className={styles.raindrop}>
+            {/* <div ref={rainBox} className={styles.raindrop}>
                 <Lottie options={rainObjectOptions} height={600} width={600} />
-            </div>
-            {/* <Lottie ref={rain} options={rainAnimationOptions} height={400} width={400} /> */}
+            </div> */}
+            <Lottie ref={rain} options={rainAnimationOptions} height={400} width={400} />
             
         </WeatherAnimation>);
     };
@@ -116,18 +129,14 @@ const Home = () => {
     const notiClick = () => {   // noti 버튼을 클릭 했을 경우,
         // 알림 페이지로 라우팅
         console.log("clicked - noti");
-
     }
 
     const flowerNameClick = () => {
         console.log("clicked - name")
     }
 
-    const rainCount = 999;    // 빗물 count 값
-    const sunCount = 999; // 햇빛 count 값
     const FlowerMessage = "안녕! 나는 튤립이야!";
-    const FlowerName = "사랑이";
-    const growPercentValue = 0; // percent value
+
     const growProgressBackgroundColor = "black";    // 프로그래스바 색상 string
 
     const sunBox = useRef();
@@ -144,18 +153,18 @@ const Home = () => {
         if (elementStatus === 'sun') {
             console.log("useEffect");
             setRainAnimation(null);
-            let timeLine = gsap.timeline();
-            timeLine.fromTo(sunBox.current, { y: "-2000" }, {
+            let sunTimeLine = gsap.timeline();
+            sunTimeLine.fromTo(sunBox.current, { y: "-2000" }, {
                 y: "-600"
             });
-            timeLine.fromTo(sunshine.current, { y: "-1000" }, {
+            sunTimeLine.fromTo(sunshine.current, { y: "-1000" }, {
                 y: "0",
                 duration: 2
             });
-            timeLine.to(sunshine.current, {
+            sunTimeLine.to(sunshine.current, {
                 y: "-2000",
             });
-            timeLine.to(sunBox.current, { y: "-2000" });
+            sunTimeLine.to(sunBox.current, { y: "-2000" });
             setElementStatus('');
             // setTimeout(function() {
             //     if(Animation !== null){
@@ -165,12 +174,12 @@ const Home = () => {
         }
         else if(elementStatus === 'rain'){
             setSunAnimation(null);
-            let timeLine = gsap.timeline();
-            timeLine.fromTo(rainBox.current, { y: "-1000" }, {
+            let rainTimeLine = gsap.timeline();
+            rainTimeLine.fromTo(rainBox.current, { y: "-1000" }, {
                 y: "0",
                 duration: 2
             });
-            timeLine.to(rainBox.current, { y: "-2000" });
+            rainTimeLine.to(rainBox.current, { y: "-2000" });
             // timeLine.fromTo(rain.current, {y: "0"}, {y: "-2000"});
             setElementStatus('');
         }
@@ -194,19 +203,19 @@ const Home = () => {
                 </div>
                 <div className={styles.FlowerInfo}>
                     <div className={styles.FlowerName} onClick={flowerNameClick}>
-                        <span className={styles.FlowerNameText}>{FlowerName}</span>
-                        <EditIcon />
+                        <span className={styles.FlowerNameText}>{flowerInfo.name}</span>
+                        <EditIcon width={15} />
                     </div>
-                    <GrowProgressBar growPercent={growPercentValue}></GrowProgressBar>
+                    <GrowProgressBar CurrentGrowthValue={flowerInfo.CurrentGrowthValue} MaxGrowthValue={flowerInfo.MaxGrowthValue}></GrowProgressBar>
                 </div>
                 <div className={styles.FlowerItem}>
                     <div className={styles.RainDiv} onClick={rainClick}>
                         <img className={styles.SunRainImg} src={require('../../assets/HomeAsset/rain-img.png')} />
-                        <div className={styles.SunRainText}>{rainCount}</div>
+                        <div className={styles.SunRainText}>{flowerInfo.rainElementCount}</div>
                     </div>
                     <div className={styles.SunDiv} onClick={sunClick}>
                         <img className={styles.SunRainImg} src={require('../../assets/HomeAsset/sun-img.png')} />
-                        <div className={styles.SunRainText}>{sunCount}</div>
+                        <div className={styles.SunRainText}>{flowerInfo.sunElementCount}</div>
                     </div>
                 </div>
                 <div className={styles.Flowerpot}>
