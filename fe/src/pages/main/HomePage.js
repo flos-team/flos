70fb3ca5910bedfc5c5e8ff82edf6c07fe2a7711
김주하 <zouamare@gmail.com>
2023-02-04 22:29:42 +0900

    feat: 홈 화면 이름 변경 기능 추가, 배경화면 변경 기능 추가

--- fe/src/pages/main/HomePage.js
+++ fe/src/pages/main/HomePage.js
@@ -13,12 +13,15 @@ import rainObjectData from "../../assets/HomeAsset/53484-digital-clouds-rain.jso
 import rainAnimationData from "../../assets/HomeAsset/81756-rain.json";
 import sunObjectData from "../../assets/HomeAsset/22190-sunny-day.json";
 import Lottie from "react-lottie";
+import ChangeFlowerNamemodal from "../../components/homepage/ChangeFlowerNamemodal";
+import DayBackground from "../../assets/HomeAsset/home-sample-background-img-day.jpg";
+import NightBackground from "../../assets/HomeAsset/home-sample-background-img-night.png";
 
 const Title = styled.h1`
   color: #007bff;
   font-size: 32px;
   ${(props) =>
-    `transform: translateY(${props.translateY}px); 
+        `transform: translateY(${props.translateY}px); 
    opacity: ${props.opacity};
   `}
 `;
@@ -29,219 +32,282 @@ const WeatherAnimation = styled.div`
   hegiht: 100%;
 `;
 
-const makeNewSeed = () => {};
+const HomePageDiv = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 0px 20px;
+    gap: 8px;
+    isolation: isolate;
+    position: relative;
+    
+    background-image: url(${(p) => p.url});
+    background-repeat:no-repeat;
+    background-size: cover;
+    overflow: hidden;
+`;
+
+
+const makeNewSeed = () => { };
 
 const flowering = () => {
-  console.log("개화 시작");
-  return (
-    <>
-      <button>개화</button>
-    </>
-  );
+    console.log("개화 시작");
+    return (
+        <>
+            <button>개화</button>
+        </>
+    );
 };
 
-const Home = () => {
-  const [flowerInfo, setFlowerInfo] = useState({
-    existence: "", // 존재 여부
-    name: "", // 이름
-    sunElementCount: 0,
-    rainElementCount: 0,
-    CurrentGrowthValue: 0,
-    MaxGrowthValue: 100,
-  });
 
-  useEffect(() => {
-    // --------------------- 꽃 상태 받아옴 (empty/ normal / fullfilled) 시작 ---------------------
-
-    setFlowerInfo({
-      existence: "fulfilled", // 존재 여부
-      name: "명식이", // 이름
-      sunElementCount: 20, // 태양 요소 개수
-      rainElementCount: 20, // 빗물 요소 개수
-      CurrentGrowthValue: 60, // 현재 고정 값
-      MaxGrowthValue: 100, // 최대 값
-    }); // flower 상태 일반 상태로 부여
-
-    // --------------------- 꽃 상태 받아옴 (empty/ normal / fullfilled) 끝 ---------------------
-
-    /*
-            꽃 정보에 따른 추가 작업
-        */
-    if (flowerInfo.flowerExistence === "empty") {
-      // 현재 키우고 있는 꽃이 없을 경우
-      makeNewSeed(); // 새로운 꽃을 만든다.
-    } else if (flowerInfo.flowerExistence === "fulfilled") {
-      // 현재 키우고 있는 꽃이 있고 게이지가 가득 찬 경우
-      flowering();
+// 낮인지 확인하는 메소드
+const isDay = (hour) => {
+    if (hour >= 6 && hour < 18) {
+        return true;
     }
-  }, []);
+    else {
+        return false;
+    }
+}
 
-  const sunClick = () => {
-    // 해 버튼을 클릭 했을 경우,
-    console.log("clicked - sun");
-    setElementImg(require("../../assets/HomeAsset/sun-img.png"));
-    setElementStatus("sun");
-    const sunObjectOptions = {
-      autoplay: true,
-      animationData: sunObjectData,
-    };
-    // 서버랑 통신해서 sun이 소진되었다고 전송
-    flowerInfo.sunElementCount--;
-    setSunAnimation(
-      <>
-        <div ref={sunBox}>
-          <Lottie options={sunObjectOptions} height={1000} width={1000} />
-        </div>
-        <div className={styles.SunShine} ref={sunshine}></div>
-      </>
-    );
-  };
+const Home = () => {
+    const FlowerMessage = "안녕! 나는 튤립이야!";
 
-  const rainClick = () => {
-    // 비 버튼을 클릭 했을 경우,
-    console.log("clicked - rain");
-    setElementImg(require("../../assets/HomeAsset/rain-img.png"));
-    setElementStatus("rain");
-    const rainAnimationOptions = {
-      // loop: true,
-      autoplay: true,
-      animationData: rainAnimationData,
+    const sunBox = useRef();
+    const rainBox = useRef();
+    const sunshine = useRef();
+    const rain = useRef();
+    const [elementStatus, setElementStatus] = useState("");
+    const [sunAnimation, setSunAnimation] = useState(null);
+    const [rainAnimation, setRainAnimation] = useState(null);
+    const [changeFlowerNamemodal, setChangeFlowerNamemodal] = useState(false);
+    let backgroundImgUrl;
+
+    const [elementImg, setElementImg] = useState(require("../../assets/HomeAsset/sun-img.png"));
+    const [flowerInfo, setFlowerInfo] = useState({
+        existence: "", // 존재 여부
+        name: "", // 이름
+        sunElementCount: 0,
+        rainElementCount: 0,
+        CurrentGrowthValue: 0,
+        MaxGrowthValue: 100,
+    });
+
+    // ----------------------- 시간에 따른 배경화면 지정 -------------------------
+    let currentHour = 20;
+    if (isDay(currentHour)) {
+        backgroundImgUrl = DayBackground;
+    }
+    else {
+        backgroundImgUrl = NightBackground;
+    }
+
+    useEffect(() => {
+        // --------------------- 꽃 상태 받아옴 (empty/ normal / fullfilled) 시작 ---------------------
+
+        setFlowerInfo({
+            existence: "fulfilled", // 존재 여부
+            name: "명식이", // 이름
+            sunElementCount: 20, // 태양 요소 개수
+            rainElementCount: 20, // 빗물 요소 개수
+            CurrentGrowthValue: 60, // 현재 고정 값
+            MaxGrowthValue: 100, // 최대 값
+        }); // flower 상태 일반 상태로 부여
+
+        // --------------------- 꽃 상태 받아옴 (empty/ normal / fullfilled) 끝 ---------------------
+
+        /*
+                꽃 정보에 따른 추가 작업
+            */
+        if (flowerInfo.flowerExistence === "empty") {
+            // 현재 키우고 있는 꽃이 없을 경우
+            makeNewSeed(); // 새로운 꽃을 만든다.
+        } else if (flowerInfo.flowerExistence === "fulfilled") {
+            // 현재 키우고 있는 꽃이 있고 게이지가 가득 찬 경우
+            flowering();
+        }
+
+    }, []);
+
+    const sunClick = () => {
+        // 해 버튼을 클릭 했을 경우,
+        console.log("clicked - sun");
+        setElementImg(require("../../assets/HomeAsset/sun-img.png"));
+        setElementStatus("sun");
+        const sunObjectOptions = {
+            autoplay: true,
+            animationData: sunObjectData,
+        };
+        if (flowerInfo.sunElementCount < 1) {
+            alert("햇빛의 양이 부족합니다.")
+            return;
+        }
+        // 서버랑 통신해서 sun이 소진되었다고 전송
+        flowerInfo.sunElementCount--;
+        flowerInfo.CurrentGrowthValue++;
+        setSunAnimation(
+            <>
+                <div ref={sunBox}>
+                    <Lottie options={sunObjectOptions} height={1000} width={1000} />
+                </div>
+                <div className={styles.SunShine} ref={sunshine}></div>
+            </>
+        );
     };
-    // 서버랑 통신해서 rain이 소진되었다고 전송
-    flowerInfo.rainElementCount--;
-    const rainObjectOptions = {
-      autoplay: true,
-      animationData: rainObjectData,
-    };
-    setRainAnimation(
-      <WeatherAnimation>
-        {/* <div ref={rainBox} className={styles.raindrop}>
+
+    const rainClick = () => {
+        // 비 버튼을 클릭 했을 경우,
+        console.log("clicked - rain");
+        setElementImg(require("../../assets/HomeAsset/rain-img.png"));
+        setElementStatus("rain");
+        const rainAnimationOptions = {
+            // loop: true,
+            autoplay: true,
+            animationData: rainAnimationData,
+        };
+        // 서버랑 통신해서 rain이 소진되었다고 전송
+        if (flowerInfo.rainElementCount < 1) {
+            alert("빗물의 양이 부족합니다.")
+            return;
+        }
+        flowerInfo.rainElementCount--;
+        flowerInfo.CurrentGrowthValue++;
+        const rainObjectOptions = {
+            autoplay: true,
+            animationData: rainObjectData,
+        };
+        setRainAnimation(
+            <WeatherAnimation>
+                {/* <div ref={rainBox} className={styles.raindrop}>
                 <Lottie options={rainObjectOptions} height={600} width={600} />
             </div> */}
-        <Lottie ref={rain} options={rainAnimationOptions} height={400} width={400} />
-      </WeatherAnimation>
+                <Lottie ref={rain} options={rainAnimationOptions} height={400} width={400} />
+            </WeatherAnimation>
+        );
+    };
+
+    const guideClick = () => {
+        // info 버튼을 클릭 했을 경우,
+        // 도움말 페이지로 라우팅
+        console.log("clicked - info");
+    };
+
+    const notiClick = () => {
+        // noti 버튼을 클릭 했을 경우,
+        // 알림 페이지로 라우팅
+        console.log("clicked - noti");
+    };
+
+    const flowerNameClick = () => {
+        console.log("clicked - name");
+        setSunAnimation(null);
+        setRainAnimation(null);
+        setChangeFlowerNamemodal(true);
+    };
+
+    const ChangeFlowerNameOnclick = (newName) => {
+        console.log("바꾸기 시작");
+        console.log("새로운 이름", newName);
+        flowerInfo.name = newName;
+        setChangeFlowerNamemodal(false);
+    };
+
+    const CancelChangingFlowerNameOnclick = () => {
+        setChangeFlowerNamemodal(false);
+    };
+
+    useEffect(() => {
+        if (elementStatus === "sun") {
+            console.log("useEffect");
+            setRainAnimation(null);
+            let sunTimeLine = gsap.timeline();
+            sunTimeLine.fromTo(
+                sunBox.current,
+                { y: "-2000" },
+                {
+                    y: "-600",
+                }
+            );
+            sunTimeLine.fromTo(
+                sunshine.current,
+                { y: "-1000" },
+                {
+                    y: "0",
+                    duration: 2,
+                }
+            );
+            sunTimeLine.to(sunshine.current, {
+                y: "-2000",
+            });
+            sunTimeLine.to(sunBox.current, { y: "-2000" });
+            setElementStatus("");
+            // setTimeout(function() {
+            //     if(Animation !== null){
+            //         setAnimation(null);
+            //     }
+            //   }, 10000);
+        } else if (elementStatus === "rain") {
+            setSunAnimation(null);
+            let rainTimeLine = gsap.timeline();
+            rainTimeLine.fromTo(
+                rainBox.current,
+                { y: "-1000" },
+                {
+                    y: "0",
+                    duration: 2,
+                }
+            );
+            rainTimeLine.to(rainBox.current, { y: "-2000" });
+            // timeLine.fromTo(rain.current, {y: "0"}, {y: "-2000"});
+            setElementStatus("");
+        }
+    }, [elementStatus]);
+
+    return (
+        <HomePageDiv url={backgroundImgUrl}>
+            <div className={styles.HomeRoot}>
+                {
+                    changeFlowerNamemodal == true ? <ChangeFlowerNamemodal oldName={flowerInfo.name} changeOnclick={ChangeFlowerNameOnclick} cancelOnclick={CancelChangingFlowerNameOnclick} /> : null
+                }
+                {sunAnimation}
+                {rainAnimation}
+                <div className={styles.HomeHeader}>
+                    <Link to="/guide">
+                        <Info onClick={guideClick} />
+                    </Link>
+                    <Link to="/notification">
+                        <Noti onClick={notiClick} />
+                    </Link>
+                </div>
+                <div className={styles.FlowerMessage}>
+                    <div className={styles.FlowerMessageText}>{FlowerMessage}</div>
+                </div>
+                <div className={styles.FlowerInfo}>
+                    <div className={styles.FlowerName}>
+                        <span className={styles.FlowerNameText}>{flowerInfo.name}</span>
+                        <EditIcon width={15} onClick={flowerNameClick} />
+                    </div>
+                    <GrowProgressBar
+                        CurrentGrowthValue={flowerInfo.CurrentGrowthValue}
+                        MaxGrowthValue={flowerInfo.MaxGrowthValue}
+                    ></GrowProgressBar>
+                </div>
+                <div className={styles.FlowerItem}>
+                    <div className={styles.RainDiv} onClick={rainClick}>
+                        <img className={styles.SunRainImg} src={require("../../assets/HomeAsset/rain-img.png")} />
+                        <div className={styles.SunRainText}>{flowerInfo.rainElementCount}</div>
+                    </div>
+                    <div className={styles.SunDiv} onClick={sunClick}>
+                        <img className={styles.SunRainImg} src={require("../../assets/HomeAsset/sun-img.png")} />
+                        <div className={styles.SunRainText}>{flowerInfo.sunElementCount}</div>
+                    </div>
+                </div>
+                <div className={styles.Flowerpot}>
+                    <Flower></Flower>
+                </div>
+            </div>
+        </HomePageDiv>
     );
-  };
-
-  const guideClick = () => {
-    // info 버튼을 클릭 했을 경우,
-    // 도움말 페이지로 라우팅
-    console.log("clicked - info");
-  };
-
-  const notiClick = () => {
-    // noti 버튼을 클릭 했을 경우,
-    // 알림 페이지로 라우팅
-    console.log("clicked - noti");
-  };
-
-  const flowerNameClick = () => {
-    console.log("clicked - name");
-  };
-
-  const FlowerMessage = "안녕! 나는 튤립이야!";
-
-  const growProgressBackgroundColor = "black"; // 프로그래스바 색상 string
-
-  const sunBox = useRef();
-  const rainBox = useRef();
-  const sunshine = useRef();
-  const rain = useRef();
-  const [elementStatus, setElementStatus] = useState("");
-  const [sunAnimation, setSunAnimation] = useState(null);
-  const [rainAnimation, setRainAnimation] = useState(null);
-
-  const [elementImg, setElementImg] = useState(require("../../assets/HomeAsset/sun-img.png"));
-
-  useEffect(() => {
-    if (elementStatus === "sun") {
-      console.log("useEffect");
-      setRainAnimation(null);
-      let sunTimeLine = gsap.timeline();
-      sunTimeLine.fromTo(
-        sunBox.current,
-        { y: "-2000" },
-        {
-          y: "-600",
-        }
-      );
-      sunTimeLine.fromTo(
-        sunshine.current,
-        { y: "-1000" },
-        {
-          y: "0",
-          duration: 2,
-        }
-      );
-      sunTimeLine.to(sunshine.current, {
-        y: "-2000",
-      });
-      sunTimeLine.to(sunBox.current, { y: "-2000" });
-      setElementStatus("");
-      // setTimeout(function() {
-      //     if(Animation !== null){
-      //         setAnimation(null);
-      //     }
-      //   }, 10000);
-    } else if (elementStatus === "rain") {
-      setSunAnimation(null);
-      let rainTimeLine = gsap.timeline();
-      rainTimeLine.fromTo(
-        rainBox.current,
-        { y: "-1000" },
-        {
-          y: "0",
-          duration: 2,
-        }
-      );
-      rainTimeLine.to(rainBox.current, { y: "-2000" });
-      // timeLine.fromTo(rain.current, {y: "0"}, {y: "-2000"});
-      setElementStatus("");
-    }
-  }, [elementStatus]);
-
-  return (
-    <div className={styles.HomePage}>
-      <div className={styles.HomeRoot}>
-        {sunAnimation}
-        {rainAnimation}
-        <div className={styles.HomeHeader}>
-          <Link to="/guide">
-            <Info onClick={guideClick} />
-          </Link>
-          <Link to="/notification">
-            <Noti onClick={notiClick} />
-          </Link>
-        </div>
-        <div className={styles.FlowerMessage}>
-          <div className={styles.FlowerMessageText}>{FlowerMessage}</div>
-        </div>
-        <div className={styles.FlowerInfo}>
-          <div className={styles.FlowerName} onClick={flowerNameClick}>
-            <span className={styles.FlowerNameText}>{flowerInfo.name}</span>
-            <EditIcon width={15} />
-          </div>
-          <GrowProgressBar
-            CurrentGrowthValue={flowerInfo.CurrentGrowthValue}
-            MaxGrowthValue={flowerInfo.MaxGrowthValue}
-          ></GrowProgressBar>
-        </div>
-        <div className={styles.FlowerItem}>
-          <div className={styles.RainDiv} onClick={rainClick}>
-            <img className={styles.SunRainImg} src={require("../../assets/HomeAsset/rain-img.png")} />
-            <div className={styles.SunRainText}>{flowerInfo.rainElementCount}</div>
-          </div>
-          <div className={styles.SunDiv} onClick={sunClick}>
-            <img className={styles.SunRainImg} src={require("../../assets/HomeAsset/sun-img.png")} />
-            <div className={styles.SunRainText}>{flowerInfo.sunElementCount}</div>
-          </div>
-        </div>
-        <div className={styles.Flowerpot}>
-          <Flower></Flower>
-        </div>
-      </div>
-    </div>
-  );
 };
 
 export default Home;
--- fe/src/pages/main/ProfilePage/ProfilePage.js
+++ fe/src/pages/main/ProfilePage/ProfilePage.js
@@ -1,145 +1,145 @@
-import { render } from "@testing-library/react";
+// import { render } from "@testing-library/react";
 
-import axios from "axios";
+// import axios from "axios";
 
-/* libraray */
-// 날짜 처리를 위한 라이브러리
-import dayjs from "dayjs";
+// /* libraray */
+// // 날짜 처리를 위한 라이브러리
+// import dayjs from "dayjs";
 
-// import React from "react";
-import { useState, useEffect, useCallback, useRef } from "react";
-/* 컴포넌트에서 사용할 이미지 에셋 import */
-import userImg from "../../../assets/GlobalAsset/user-img.png";
+// // import React from "react";
+// import { useState, useEffect, useCallback, useRef } from "react";
+// /* 컴포넌트에서 사용할 이미지 에셋 import */
+// import userImg from "../../../assets/GlobalAsset/user-img.png";
 
-/* import component */
-import HeaderComponent from "../../../components/HeaderComponent/HeaderComponent";
-import PostItem from "../../../components/PostItem/PostItem";
+// /* import component */
+// import HeaderComponent from "../../../components/HeaderComponent/HeaderComponent";
+// import PostItem from "../../../components/PostItem/PostItem";
 
-/* import module */
-import { getPost } from "./AxiosModule";
-import { getTimeDiffText } from "../../../api/DateModule";
-import MemberAPI, {
-  getCheckEmail,
-  getCheckNickname,
-  getMemberInfo,
-  logout,
-  sendCodeToEmail,
-  doLogin,
-  signUpUser,
-  modifyUserInfo,
-  resetUserPassword,
-  withdrawalUser,
-} from "../../../api/MemberAPI";
+// /* import module */
+// import { getPost } from "./AxiosModule";
+// import { getTimeDiffText } from "../../../api/DateModule";
+// import MemberAPI, {
+//   getCheckEmail,
+//   getCheckNickname,
+//   getMemberInfo,
+//   logout,
+//   sendCodeToEmail,
+//   doLogin,
+//   signUpUser,
+//   modifyUserInfo,
+//   resetUserPassword,
+//   withdrawalUser,
+// } from "../../../api/MemberAPI";
 
-/* Profile Page 전용 CSS import */
-import "./ProfilePage.css";
+// /* Profile Page 전용 CSS import */
+// import "./ProfilePage.css";
 
-const ProfilePage = () => {
-  const [postIdx, setPostIdx] = useState(1);
-  const [postList, setPostList] = useState([]);
-  const [isScrollable, setIsScrollable] = useState(true);
-  let scRef = useRef(1);
-  // 무한 스크롤 함수;
-  const renderPostList = useCallback(
-    (array) => {
-      let getPostList = [...array].map((e, i) => {
-        // console.dir(e);
-        let { content, writer, regDate } = e;
-        let postDay = dayjs(regDate, "YYYY-MM-DD HH:mm:ss");
-        let curDay = dayjs(new Date(), "YYYY-MM-DD HH:mm:ss");
-        let text = getTimeDiffText(postDay, curDay);
-        return <PostItem mood={"RAINY"} userName={writer.nickname} postText={content} postTimeLog={text}></PostItem>;
-      });
-      let newPostList = postList.concat(getPostList);
-      setPostList(newPostList);
-    },
-    [postList]
-  ); // postList가 바뀌었을 때만 작동!
+// const ProfilePage = () => {
+//   const [postIdx, setPostIdx] = useState(1);
+//   const [postList, setPostList] = useState([]);
+//   const [isScrollable, setIsScrollable] = useState(true);
+//   let scRef = useRef(1);
+//   // 무한 스크롤 함수;
+//   const renderPostList = useCallback(
+//     (array) => {
+//       let getPostList = [...array].map((e, i) => {
+//         // console.dir(e);
+//         let { content, writer, regDate } = e;
+//         let postDay = dayjs(regDate, "YYYY-MM-DD HH:mm:ss");
+//         let curDay = dayjs(new Date(), "YYYY-MM-DD HH:mm:ss");
+//         let text = getTimeDiffText(postDay, curDay);
+//         return <PostItem mood={"RAINY"} userName={writer.nickname} postText={content} postTimeLog={text}></PostItem>;
+//       });
+//       let newPostList = postList.concat(getPostList);
+//       setPostList(newPostList);
+//     },
+//     [postList]
+//   ); // postList가 바뀌었을 때만 작동!
 
-  // useEffect(() => {
-  //   if (isScrollable) {
-  //     let data = getPost();
-  //     data
-  //       .then((response) => {
-  //         // console.dir(response);
-  //         renderPostList(response);
-  //         setPostIdx(postIdx + 1);
-  //       })
-  //       .catch((e) => {
-  //         console.log("에러감지");
-  //         alert("불러올 데이터가 없습니다");
-  //       });
-  //   }
-  // }, []);
+//   // useEffect(() => {
+//   //   if (isScrollable) {
+//   //     let data = getPost();
+//   //     data
+//   //       .then((response) => {
+//   //         // console.dir(response);
+//   //         renderPostList(response);
+//   //         setPostIdx(postIdx + 1);
+//   //       })
+//   //       .catch((e) => {
+//   //         console.log("에러감지");
+//   //         alert("불러올 데이터가 없습니다");
+//   //       });
+//   //   }
+//   // }, []);
 
-  const handleScroll = (e) => {
-    const bottom = e.target.scrollHeight - e.target.scrollTop === e.target.clientHeight;
-    if (bottom && isScrollable) {
-      console.log("스크롤의 끝 감지");
-      let data = getPost(postIdx);
-      data
-        .then((response) => {
-          // console.dir(response);
-          renderPostList(response);
-          setPostIdx(postIdx + 1);
-        })
-        .catch((e) => {
-          console.log("에러감지");
-          alert("불러올 데이터가 없습니다");
-          setIsScrollable(false);
-        });
-    }
-  };
+//   const handleScroll = (e) => {
+//     const bottom = e.target.scrollHeight - e.target.scrollTop === e.target.clientHeight;
+//     if (bottom && isScrollable) {
+//       console.log("스크롤의 끝 감지");
+//       let data = getPost(postIdx);
+//       data
+//         .then((response) => {
+//           // console.dir(response);
+//           renderPostList(response);
+//           setPostIdx(postIdx + 1);
+//         })
+//         .catch((e) => {
+//           console.log("에러감지");
+//           alert("불러올 데이터가 없습니다");
+//           setIsScrollable(false);
+//         });
+//     }
+//   };
 
-  return (
-    <>
-      <HeaderComponent
-        backVisible={false}
-        pageName={"프로필"}
-        menuOpt2={"SETTING"}
-        menuOpt1={"STATISTICS"}
-      ></HeaderComponent>
-      <div className="request-div" ref={scRef}>
-        <button
-          style={{ width: "100px", height: "30px" }}
-          onClick={(e) => {
-            doLogin("onehee@ssafy.com", "dnjsgml1234");
-          }}
-        >
-          로그인
-        </button>
-        <button
-          style={{ width: "100px", height: "30px" }}
-          onClick={(e) => {
-            // "onehee@ssafy.com"
-            sendCodeToEmail("onehee@ssafy.com");
-          }}
-        >
-          로그아웃
-        </button>
-      </div>
-      <div className="test-container" onScroll={handleScroll}>
-        {postList}
-      </div>
-      {/* <div className="profile-page">
-        <img className="user-profile-img" src={userImg}></img>
-        <div className="user-info-container">
-          <div className="user-social-info-box">
-            <ul className="user-social-info-title">{userInfoNameList}</ul>
-            <ul className="user-social-info">{userInfoList}</ul>
-          </div>
-        </div>
-        <div className="profile-tab-menu">newPostList
-          <ul className="tab-menus">
-            <li className="post-menu">포스트</li>
-            <li className="book-menu">북마크</li>
-          </ul>
-        </div>
-        <div className="feed-container">{postList}</div>
-      </div> */}
-      {/* <PostDetailPage></PostDetailPage> */}
-    </>
-  );
-};
+//   return (
+//     <>
+//       <HeaderComponent
+//         backVisible={false}
+//         pageName={"프로필"}
+//         menuOpt2={"SETTING"}
+//         menuOpt1={"STATISTICS"}
+//       ></HeaderComponent>
+//       <div className="request-div" ref={scRef}>
+//         <button
+//           style={{ width: "100px", height: "30px" }}
+//           onClick={(e) => {
+//             doLogin("onehee@ssafy.com", "dnjsgml1234");
+//           }}
+//         >
+//           로그인
+//         </button>
+//         <button
+//           style={{ width: "100px", height: "30px" }}
+//           onClick={(e) => {
+//             // "onehee@ssafy.com"
+//             sendCodeToEmail("onehee@ssafy.com");
+//           }}
+//         >
+//           로그아웃
+//         </button>
+//       </div>
+//       <div className="test-container" onScroll={handleScroll}>
+//         {postList}
+//       </div>
+//       {/* <div className="profile-page">
+//         <img className="user-profile-img" src={userImg}></img>
+//         <div className="user-info-container">
+//           <div className="user-social-info-box">
+//             <ul className="user-social-info-title">{userInfoNameList}</ul>
+//             <ul className="user-social-info">{userInfoList}</ul>
+//           </div>
+//         </div>
+//         <div className="profile-tab-menu">newPostList
+//           <ul className="tab-menus">
+//             <li className="post-menu">포스트</li>
+//             <li className="book-menu">북마크</li>
+//           </ul>
+//         </div>
+//         <div className="feed-container">{postList}</div>
+//       </div> */}
+//       {/* <PostDetailPage></PostDetailPage> */}
+//     </>
+//   );
+// };
 
-export default ProfilePage;
+// export default ProfilePage;
