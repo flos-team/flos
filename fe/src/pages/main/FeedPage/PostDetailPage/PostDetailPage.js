64124ab90213376042210a251f86c9f7a064e925
김지환 <tykimdream@naver.com>
2023-02-05 00:21:50 +0900

    feat : PostDetailPage axios

--- fe/src/pages/main/FeedPage/PostDetailPage/PostDetailPage.js
+++ fe/src/pages/main/FeedPage/PostDetailPage/PostDetailPage.js
@@ -1,6 +1,10 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import axios from "axios";
+import { getPost } from "../../../../api/PostAPI";
+import { getTimeDiffText } from "../../../../api/DateModule";
+
+import dayjs from "dayjs";
+
 
 /* img import */
 import writerProfileSample from "../../../../assets/DummyData/writerProfileSample.png";
@@ -20,19 +24,12 @@ const PostDetailPage = () => {
   const [post, setPost] = useState([]);
 
   useEffect(() => {
-    axios
-      .get(`/post/${params.id}`)
-      .then((response) => {
-        // setPosts(response.data.content);
-        console.log(response.data);
-      })
-      .catch((error) => {
-        console.log("error : " + error);
-        console.dir(axios.defaults);
-      });
+    getPost(params.id).then((response) => {
+      // console.log(response);
+      setPost(response);
+    });
   }, []);
-
-
+  console.log(post)
   let commentItem = (
     <div className="comment-item">
       <img className="user-img" src={writerProfileSample} />
@@ -41,41 +38,54 @@ const PostDetailPage = () => {
           <p>babdoduk</p>
           <p>34분 전</p>
         </div>
-        <div className="comment-main">아직도 난 너를 느끼죠 이렇게 우리 지금이 순간도 난 그대가 보여 길가에 덩그러니 놓여진 저 의자 위에도
-                밤을 타고 쓸쓸히 춤추는 전 낙엽 위에도 뺨을 스치는 어느 저녁의 그 공기 속에도 길가에 내려앉은 음악속에도
-                네가 있어 어떤가요 그대 어떤가요 그대 그대는 지웠을 텐데 어떤가요 그대 우린
-                라랄라라라라라라라라라라라라라라라라라라라라라라ㅏ라라라라라라랄</div>
+        <div className="comment-main">
+          아직도 난 너를 느끼죠 이렇게 우리 지금이 순간도 난 그대가 보여 길가에
+          덩그러니 놓여진 저 의자 위에도 밤을 타고 쓸쓸히 춤추는 전 낙엽 위에도
+          뺨을 스치는 어느 저녁의 그 공기 속에도 길가에 내려앉은 음악속에도 네가
+          있어 어떤가요 그대 어떤가요 그대 그대는 지웠을 텐데 어떤가요 그대 우린
+          라랄라라라라라라라라라라라라라라라라라라라라라라ㅏ라라라라라라랄
+        </div>
       </div>
       <img className="check-btn" src={sunnyActivate} />
     </div>
   );
   const commentItemList = [...Array(8)].map((e, i) => commentItem);
 
+  let postDay = dayjs(post.regDate, "YYYY-MM-DD HH:mm:ss");
+  let curDay = dayjs(new Date(), "YYYY-MM-DD HH:mm:ss");
+  let RegBefore = getTimeDiffText(postDay, curDay);
+
   return (
     <>
       <div className="post-detail-page">
-      <HeaderComponent backVisible={true} pageName={"피드"} optType={0}></HeaderComponent>
+        <HeaderComponent
+          backVisible={true}
+          pageName={"피드"}
+          optType={0}
+        ></HeaderComponent>
         <div className="post-detail-container">
           <div className="post-container">
             <div className="user-info-container">
               <div className="user-info-div">
-                <img src={commentProfileSample} />
+                <img src={post.writer.picture} />
                 <div className="text-div">
-                  <p className="user-name">olivia</p>
-                  <p className="time-log">45분 전 ☀️</p>
+                  <p className="user-name">{post.writer.nickname}</p>
+                  <p className="time-log">{RegBefore} ☀️</p>
                 </div>
               </div>
             </div>
-            <div className="img-container" style={{ backgroundImage: `url(${writerProfileSample})` }}></div>
+            <div
+              className="img-container"
+              style={{ backgroundImage: `url(${writerProfileSample})` }}
+            ></div>
             <div className="post-content-container">
               <div className="post-text-div">
-                본문 아직도 난 너를 느끼죠 이렇게 우리 지금이 순간도 난 그대가 보여 길가에 덩그러니 놓여진 저 의자 위에도
-                밤을 타고 쓸쓸히 춤추는 전 낙엽 위에도 뺨을 스치는 어느 저녁의 그 공기 속에도 길가에 내려앉은 음악속에도
-                네가 있어 어떤가요 그대 어떤가요 그대 그대는 지웠을 텐데 어떤가요 그대 우린
-                라랄라라라라라라라라라라라라라라라라라라라라라라ㅏ라라라라라라랄
+                {post.content}
               </div>
               <div className="post-tag-container">
-                <div className="post-tag">한둘셋넷다여일여아열한둘셋넷다여일여아열</div>
+                <div className="post-tag">
+                  한둘셋넷다여일여아열한둘셋넷다여일여아열
+                </div>
                 <div className="post-tag">HTTP통신</div>
                 <div className="post-tag">CORS</div>
                 <div className="post-tag">프록시</div>
@@ -92,7 +102,9 @@ const PostDetailPage = () => {
           <img className="user-icon" src={writerProfileSample} />
           <div className="comment-input-div">
             <input className="comment-input" />
-            <button style={{ backgroundImage: `url(${sendCommentBtn})` }}></button>
+            <button
+              style={{ backgroundImage: `url(${sendCommentBtn})` }}
+            ></button>
           </div>
         </div>
       </div>
