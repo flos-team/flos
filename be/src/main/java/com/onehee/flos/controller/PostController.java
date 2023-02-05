602f94b6ff535b5f0a5c84d367216867c46d0ec2
potato3641 <gearg1456@gmail.com>
2023-02-05 11:49:36 +0900

    fix: tagDTO

--- be/src/main/java/com/onehee/flos/controller/PostController.java
+++ be/src/main/java/com/onehee/flos/controller/PostController.java
@@ -88,7 +88,7 @@ public class PostController {
 
     @Tag(name = "게시글API")
     @Operation(summary = "게시글 수정", description = "게시글 내용을 수정합니다.")
-    @PostMapping("/modify")
+    @PutMapping("/modify")
     public ResponseEntity<?> modifyPost(@RequestBody PostModifyRequestDTO postModifyRequestDTO) throws BadRequestException, IOException {
         postService.modifyPost(postModifyRequestDTO);
         return new ResponseEntity<>(HttpStatus.OK);
--- be/src/main/java/com/onehee/flos/model/dto/PostRelationDTO.java
+++ be/src/main/java/com/onehee/flos/model/dto/PostRelationDTO.java
@@ -1,6 +1,7 @@
 package com.onehee.flos.model.dto;
 
 import com.onehee.flos.model.dto.response.FileResponseDTO;
+import com.onehee.flos.model.dto.response.TagResponseDTO;
 import com.onehee.flos.model.entity.Tag;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -14,13 +15,13 @@ import java.util.List;
 @AllArgsConstructor
 @Builder
 public class PostRelationDTO {
-    private List<Tag> tagList; // List<Tag> 게시글 태그
+    private List<TagResponseDTO> tagList; // List<Tag> 게시글 태그
     private List<FileResponseDTO> attachFiles; // List<File> 게시글 첨부파일
     private boolean isFollowed;
     private boolean isBookmarked; // Bookmark 게시글 북마크 여부
     private Long countComment;
 
-    public static PostRelationDTO toDto(List<Tag> tagList, List<FileResponseDTO> attachFiles, boolean isFollowed, boolean isBookmarked, Long countComment) {
+    public static PostRelationDTO toDto(List<TagResponseDTO> tagList, List<FileResponseDTO> attachFiles, boolean isFollowed, boolean isBookmarked, Long countComment) {
         return PostRelationDTO.builder()
                 .tagList(tagList)
                 .attachFiles(attachFiles)
