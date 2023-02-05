8b5c21d98503feddc6ffc665307d722a13755217
1-Hee <onehee9710@gmail.com>
2023-02-05 16:03:03 +0900

    style : AlarmItem에 조건부 WeatherBtnComponent 추가

--- fe/src/pages/NotificationPage/NotificationPage.js
+++ fe/src/pages/NotificationPage/NotificationPage.js
@@ -1,27 +1,44 @@
-import HeaderComponent from "../../components/HeaderComponent/HeaderComponent";
+import { useState, useEffect } from "react";
 
+import HeaderComponent from "../../components/HeaderComponent/HeaderComponent";
 /* import img css */
 import userImg from "../../assets/DummyData/writerProfileSample.png";
 
 /* import component */
-import AlarmItem from "../../components/AlarmItem/AlarmItem"
+import AlarmItem from "../../components/AlarmItem/AlarmItem";
 
 /* import css */
 import "./NotificationPage.css";
 
 const NotificationPage = () => {
-    let testText = <><b>김채원</b>님이 <b>사쿠라</b> 님의 댓글에 좋아요를 누르셨습니다.</>
-    let testTimeLog = "23시간 전"
-    let n = 10;
-    const AlarmItemList = [...Array(n)].map((e, i)=><AlarmItem AlarmImg={userImg} AlarmTextJSX={testText} AlarmTimeLog={testTimeLog} ></AlarmItem>)
-    return (
-        <div style={{backgroundColor:"#e8e8e8"}}>
-            <HeaderComponent backVisible={true} pageName={"알림"} optType={0}></HeaderComponent>
-            <div className="alarm-container">
-                {AlarmItemList}
-            </div>
-        </div>
-    );
-}
+  let testText = (
+    <>
+      <b>김채원</b>님이 <b>사쿠라</b> 님의 댓글에 좋아요를 누르셨습니다.
+    </>
+  );
+  let testTimeLog = "23시간 전";
+  const [AlarmItemList, setAlarmItemList] = useState([<></>]);
+
+  let n = 5;
+  let commonAlarmItemList = [...Array(n)].map((e, i) => (
+    <AlarmItem AlarmImg={userImg} AlarmTextJSX={testText} AlarmTimeLog={testTimeLog}></AlarmItem>
+  ));
+  let dummy = ["SUNNY", "CLOUDY", "RAINY", "SUNNY", "CLOUDY", "RAINY", "SUNNY", "CLOUDY", "RAINY"];
+  let newAlarmItemList = commonAlarmItemList.concat(
+    dummy.map((e) => (
+      <AlarmItem AlarmImg={userImg} AlarmTextJSX={testText} AlarmTimeLog={testTimeLog} weather={e}></AlarmItem>
+    ))
+  );
+  useEffect(() => {
+    setAlarmItemList(newAlarmItemList);
+  }, []);
+
+  return (
+    <div style={{ backgroundColor: "#e8e8e8" }}>
+      <HeaderComponent backVisible={true} pageName={"알림"} optType={0}></HeaderComponent>
+      <div className="alarm-container">{AlarmItemList}</div>
+    </div>
+  );
+};
 
 export default NotificationPage;
