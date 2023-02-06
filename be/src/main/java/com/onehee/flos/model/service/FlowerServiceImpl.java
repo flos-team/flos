0603082791acd1b8f820ff8d050a7759d89fd4e8
joykst96 <joykst96@gmail.com>
2023-02-06 17:44:13 +0900

    feat: 물주기, 햇빛주기 구현

--- be/src/main/java/com/onehee/flos/model/service/FlowerServiceImpl.java
+++ be/src/main/java/com/onehee/flos/model/service/FlowerServiceImpl.java
@@ -7,18 +7,22 @@ import com.onehee.flos.model.dto.request.FlowerModifyRequestDTO;
 import com.onehee.flos.model.dto.response.FlowerResponseDTO;
 import com.onehee.flos.model.entity.Flower;
 import com.onehee.flos.model.entity.Member;
+import com.onehee.flos.model.entity.WeatherResource;
+import com.onehee.flos.model.entity.type.WeatherType;
 import com.onehee.flos.model.repository.FlowerRepository;
-import com.onehee.flos.model.repository.MemberRepository;
+import com.onehee.flos.model.repository.WeatherResourceRepository;
 import com.onehee.flos.util.SecurityManager;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class FlowerServiceImpl implements FlowerService {
 
     private final FlowerRepository flowerRepository;
+    private final WeatherResourceRepository weatherResourceRepository;
 
     @Override
     public void createFlower(FlowerCreateRequestDTO flowerCreateRequestDTO) throws BadRequestException {
@@ -54,8 +58,54 @@ public class FlowerServiceImpl implements FlowerService {
     @Override
     public SliceResponseDTO getContributorByFlower(Long flowerId, Pageable pageable) throws BadRequestException {
         Flower flower = flowerRepository.findById(flowerId).orElseThrow(() -> new BadRequestException("해당하는 꽃이 없습니다."));
-//        if (!flower.getOwner().equals(SecurityManager.getCurrentMember()))
-//            throw new BadRequestException("꽃 주인이 아닙니다.");
+        if (!flower.getOwner().equals(SecurityManager.getCurrentMember()))
+            throw new BadRequestException("꽃 주인이 아닙니다.");
         return SliceResponseDTO.toDto(flowerRepository.findContributorByFlower(flower, pageable));
+    }
+
+    @Override
+    @Transactional
+    public FlowerResponseDTO giveWater() {
+        Member member = SecurityManager.getCurrentMember();
+        Flower flower = flowerRepository.findByOwnerAndBlossomAtIsNull(member);
+        if (flower == null) {
+            throw new BadRequestException("현재 키우고 있는 꽃이 없습니다.");
+        }
+
+        // 제일 오래된 사용가능한 물 가져옴
+        WeatherResource water = weatherResourceRepository.findFirstByOwnerAndWeatherTypeIsAndFlowerIsNull(member, WeatherType.RAINY)
+                .orElseThrow(() -> new BadRequestException("남은 물이 없습니다."));
+
+        // 물쓰기
+        water.setFlower(flower);
+
+        // 갱신
+        flower = flowerRepository.saveAndFlush(flower);
+
+        // 반환
+        return FlowerResponseDTO.toDto(flower);
+    }
+
+    @Override
+    @Transactional
+    public FlowerResponseDTO giveLight() {
+        Member member = SecurityManager.getCurrentMember();
+        Flower flower = flowerRepository.findByOwnerAndBlossomAtIsNull(member);
+        if (flower == null) {
+            throw new BadRequestException("현재 키우고 있는 꽃이 없습니다.");
+        }
+
+        // 제일 오래된 사용가능한 물 가져옴
+        WeatherResource light = weatherResourceRepository.findFirstByOwnerAndWeatherTypeIsAndFlowerIsNull(member, WeatherType.SUNNY)
+                .orElseThrow(() -> new BadRequestException("남은 햇빛이 없습니다."));
+
+        // 물쓰기
+        light.setFlower(flower);
+
+        // 갱신
+        flower = flowerRepository.saveAndFlush(flower);
+
+        // 반환
+        return FlowerResponseDTO.toDto(flower);
     }
 }
