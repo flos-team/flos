c4de54d8a705dd97b6d9de12aeb84e4bb8550835
potato3641 <gearg1456@gmail.com>
2023-02-06 22:11:28 +0900

    fix: post list bookmark

--- be/src/main/java/com/onehee/flos/controller/PostController.java
+++ be/src/main/java/com/onehee/flos/controller/PostController.java
@@ -73,9 +73,9 @@ public class PostController {
     public ResponseEntity<?> getListByBookmark(@RequestParam(value="page", required = false) Integer page){
         PageRequest pageRequest = null;
         if (page==null)
-            pageRequest = PageRequest.of(0, size, Sort.by("createdAt").descending());
+            pageRequest = PageRequest.of(0, size, Sort.by("created_at").descending());
         else
-            pageRequest = PageRequest.of(page, size, Sort.by("createdAt").descending());
+            pageRequest = PageRequest.of(page, size, Sort.by("created_at").descending());
         return new ResponseEntity<SliceResponseDTO>(postService.getBookmarkedListByMember(pageRequest), HttpStatus.OK);
     }
 
