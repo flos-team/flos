2a6a23ff1a9389b3e2a39a648b9df10b6c32fa33
조원희 <onehee9710@gmail.com>
2023-02-08 08:45:10 +0900

    feat: import 구문 수정1

--- fe/src/pages/main/WritePostPage/WritePostPage.js
+++ fe/src/pages/main/WritePostPage/WritePostPage.js
@@ -15,7 +15,6 @@ import HeaderComponent from "../../../components/HeaderComponent/HeaderComponent
 import PostResultModal from "../../../components/PostResultModal/PostResultModal";
 
 /* import module */
-import { getSentimentResult } from "../../../api/SentiMentAPI";
 import { createPost } from "../../../api/PostAPI";
 
 /* import css */
