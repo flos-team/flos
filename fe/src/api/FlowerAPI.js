52db4462137cb110c907c04b27f3414cf0b6e977
김주하 <zouamare@gmail.com>
2023-02-08 16:57:15 +0900

    feat: 홈 페이지 서버 연동

--- fe/src/api/FlowerAPI.js
+++ fe/src/api/FlowerAPI.js
@@ -22,6 +22,7 @@ const getFlowerInfo = async () => {
     .get(url)
     .then(({data}) => {
       value = {
+        id: data.id,
         name: data.name,
         isFullGrown: data.isFullGrown,
         flowerType: data.flowerType,
@@ -35,4 +36,86 @@ const getFlowerInfo = async () => {
   return value;
 };
 
-export { getFlowerInfo };
+/////////* POST *///////////////////
+/**
+ * createFlower : 꽃 정보를 줌
+ * @param {string} flowerType 
+ * @param {string} name 
+ */
+const createFlower = async (flowerType, name) => {
+  let url = `/api/flower`;
+  let value = null;
+  await axios
+    .post(url, {flowerType,name})
+    .then(({data}) => {
+      console.log("꽃 생성함!!");
+      console.log(data);
+    })
+    .catch((err) => {
+      console.log(err, "꽃 생성하는 중 오류 발생");
+    });
+  return value;
+};
+
+/**
+ * giveSun : sun을 하나 줌
+ */
+
+const giveSun = async () => {
+  let url = `/api/flower/give-light`;
+  let value = null;
+  await axios
+    .post(url, {})
+    .then(({data}) => {
+      console.log("햇빛 줌!!");
+      console.log(data);
+      value = data;
+    })
+    .catch((err) => {
+      console.log(err, "꽃 생성하는 중 오류 발생");
+    });
+  return value;
+};
+
+/**
+ * giveRain : rain을 하나 줌
+ */
+
+const giveRain = async () => {
+  let url = `/api/flower/give-water`;
+  let value = null;
+  await axios
+    .post(url, {})
+    .then(({data}) => {
+      console.log("비 줌!!");
+      console.log(data);
+      value = data;
+    })
+    .catch((err) => {
+      console.log(err, "꽃 생성하는 중 오류 발생");
+    });
+  return value;
+};
+
+/////////* PUT *///////////////////
+/**
+ * modifyFlower : 꽃 이름 변경 줌
+ * @param {long} id 
+ * @param {string} name 
+ */
+const modifyFlower = async (id, name) => {
+  let url = `/api/flower`;
+  let value = null;
+  await axios
+    .put(url, {id,name})
+    .then(({data}) => {
+      console.log("꽃 이름 변경함!!");
+      console.log(data);
+    })
+    .catch((err) => {
+      console.log(err, "꽃 이름 변경하는 중 오류 발생");
+    });
+  return value;
+};
+
+export { getFlowerInfo, createFlower, giveSun, giveRain, modifyFlower };
