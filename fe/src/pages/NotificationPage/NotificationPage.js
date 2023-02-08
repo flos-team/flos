97bbd4f4c7f7259010d4fc9227480c7bda6ac9cb
dlaqjarb9 <dlaqjarb9@daum.net>
2023-02-08 15:36:05 +0900

    feat: 알림 axios 기능

--- fe/src/pages/NotificationPage/NotificationPage.js
+++ fe/src/pages/NotificationPage/NotificationPage.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-
+import sampleImg from '../../assets/HomeAsset/send-letter.png'
 import HeaderComponent from "../../components/HeaderComponent/HeaderComponent";
 /* import img css */
 import userImg from "../../assets/DummyData/writerProfileSample.png";
@@ -10,33 +10,60 @@ import AlarmItem from "../../components/AlarmItem/AlarmItem";
 /* import css */
 import "./NotificationPage.css";
 
+import { getNotification } from "../../api/NotificationAPI"
+
+// const NotificationPage = () => {
+//   let testText = (
+//     <>
+//       <b>김채원</b>님이 <b>사쿠라</b> 님의 댓글에 좋아요를 누르셨습니다.
+//     </>
+//   );
+//   let testTimeLog = "23시간 전";
+//   const [AlarmItemList, setAlarmItemList] = useState([<></>]);
+
+//   let n = 5;
+//   let commonAlarmItemList = [...Array(n)].map((e, i) => (
+//     <AlarmItem AlarmImg={userImg} AlarmTextJSX={testText} AlarmTimeLog={testTimeLog}></AlarmItem>
+//   ));
+//   let dummy = ["SUNNY", "CLOUDY", "RAINY", "SUNNY", "CLOUDY", "RAINY", "SUNNY", "CLOUDY", "RAINY"];
+//   let newAlarmItemList = commonAlarmItemList.concat(
+//     dummy.map((e) => (
+//       <AlarmItem AlarmImg={userImg} AlarmTextJSX={testText} AlarmTimeLog={testTimeLog} weather={e}></AlarmItem>
+//     ))
+//   );
+  // useEffect(() => {
+  //   setAlarmItemList(newAlarmItemList);
+  // }, []);
+
+//   return (
+//     <div style={{ backgroundColor: "#e8e8e8" }}>
+//       <HeaderComponent backVisible={true} pageName={"알림"} optType={0}></HeaderComponent>
+//       <div className="alarm-container">{AlarmItemList}</div>
+//     </div>
+//   );
+// };
+
+// export default NotificationPage;
+
 const NotificationPage = () => {
-  let testText = (
-    <>
-      <b>김채원</b>님이 <b>사쿠라</b> 님의 댓글에 좋아요를 누르셨습니다.
-    </>
-  );
-  let testTimeLog = "23시간 전";
-  const [AlarmItemList, setAlarmItemList] = useState([<></>]);
 
-  let n = 5;
-  let commonAlarmItemList = [...Array(n)].map((e, i) => (
-    <AlarmItem AlarmImg={userImg} AlarmTextJSX={testText} AlarmTimeLog={testTimeLog}></AlarmItem>
-  ));
-  let dummy = ["SUNNY", "CLOUDY", "RAINY", "SUNNY", "CLOUDY", "RAINY", "SUNNY", "CLOUDY", "RAINY"];
-  let newAlarmItemList = commonAlarmItemList.concat(
-    dummy.map((e) => (
-      <AlarmItem AlarmImg={userImg} AlarmTextJSX={testText} AlarmTimeLog={testTimeLog} weather={e}></AlarmItem>
-    ))
-  );
+  const [notiList, setNotiList] = useState([]);
+
   useEffect(() => {
-    setAlarmItemList(newAlarmItemList);
-  }, []);
+    getNotification().then((res) => {
+      setNotiList(res) 
+    })
+  }, [])
 
+  const notiItemList = notiList.map(({message, createdAt}) => (
+    <AlarmItem AlarmImg={sampleImg} AlarmTextJSX={message} AlarmTimeLog={createdAt}></AlarmItem>
+  ));
+
+  
   return (
     <div style={{ backgroundColor: "#e8e8e8" }}>
       <HeaderComponent backVisible={true} pageName={"알림"} optType={0}></HeaderComponent>
-      <div className="alarm-container">{AlarmItemList}</div>
+      <div className="alarm-container">{notiItemList}</div>
     </div>
   );
 };
