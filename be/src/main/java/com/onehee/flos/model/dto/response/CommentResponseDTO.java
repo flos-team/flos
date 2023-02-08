ea9d73abf4c3e94b4d082647e68a782b5c0cd5b2
potato3641 <gearg1456@gmail.com>
2023-02-08 13:47:17 +0900

    feat: add boolean

--- be/src/main/java/com/onehee/flos/model/dto/response/CommentResponseDTO.java
+++ be/src/main/java/com/onehee/flos/model/dto/response/CommentResponseDTO.java
@@ -3,12 +3,17 @@ package com.onehee.flos.model.dto.response;
 import com.onehee.flos.model.entity.Comment;
 import com.onehee.flos.model.entity.Member;
 import com.onehee.flos.model.entity.Post;
+import com.onehee.flos.util.SecurityManager;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Formula;
 
+import javax.persistence.Basic;
+import javax.persistence.FetchType;
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Getter
 @NoArgsConstructor
@@ -24,10 +29,15 @@ public class CommentResponseDTO {
     private LocalDateTime createdAt;
     //    private LocalDateTime modifiedAt;
     private Boolean isApprove;
+    private Boolean isMine;
+    private Boolean isCommented;
 
     public static CommentResponseDTO toDto(Comment comment) {
         Long parent = null;
         Long primitive = null;
+        boolean isCommented = false;
+        if (comment.getDescendants().size() > 0)
+            isCommented = true;
         if (comment.getParent() != null)
             parent = comment.getParent().getId();
         if (comment.getPrimitive() != null)
@@ -42,6 +52,8 @@ public class CommentResponseDTO {
                 .createdAt(comment.getCreatedAt())
 //                .modifyAt(comment.getModifiedAt())
                 .isApprove(comment.getIsApprove())
+                .isMine(comment.getWriter().getId() == SecurityManager.getCurrentMember().getId())
+                .isCommented(isCommented)
                 .build();
     }
 
