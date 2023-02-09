cf7677c579c01c3071ad0bb259cfee0bc569abe3
dlaqjarb9 <dlaqjarb9@daum.net>
2023-02-09 15:29:30 +0900

    file: 404Page 추가

--- fe/src/Transition.js
+++ fe/src/Transition.js
@@ -24,6 +24,8 @@ import NotificationPage from "./pages/NotificationPage/NotificationPage";
 import GuidePage from "./pages/GuidePage";
 import EndingPage from "./pages/main/EndingPage/EndingPage"
 
+import NotFoundPage from "./pages/NotFoundPage"
+
 import "./Transition.css";
 
 function Transition() {
@@ -58,6 +60,7 @@ function Transition() {
           <Route path="/other-profile-page/:id" element={<OtherProfilePage />}></Route>
           <Route path="/follower-view-page" element={<FollowerViewPage />}></Route>
           <Route path="/flower-end-page" element={<EndingPage/>}></Route>
+          <Route path="*" element={<NotFoundPage/>}></Route>
 
           {/* <Route path="/product/*" element={<Product />}></Route> */}
           {/* 상단에 위치하는 라우트들의 규칙을 모두 확인, 일치하는 라우트가 없는경우 처리 */}
