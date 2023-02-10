f45e0cc789a4944cb60af449578478233b1a160f
potato3641 <gearg1456@gmail.com>
2023-02-10 10:46:13 +0900

    feat: fix post dto

--- be/src/main/java/com/onehee/flos/model/dto/request/PostCreateRequestDTO.java
+++ be/src/main/java/com/onehee/flos/model/dto/request/PostCreateRequestDTO.java
@@ -6,23 +6,20 @@ import com.onehee.flos.model.entity.Member;
 import com.onehee.flos.model.entity.Post;
 import com.onehee.flos.model.entity.Tag;
 import com.onehee.flos.model.entity.type.WeatherType;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
 @Getter
-@NoArgsConstructor
-@AllArgsConstructor
+@Setter
+@RequiredArgsConstructor
 @Builder
 public class PostCreateRequestDTO {
-    private String content;
-    private WeatherType weather;
-    private List<MultipartFile> attachFiles;
-    private List<String> tagList;
+    private final String content;
+    private final WeatherType weather;
+    private final List<MultipartFile> attachFiles;
+    private final List<String> tagList;
 
     public Post toEntity(Member writer) {
         return Post.builder()
