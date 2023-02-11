20acc6d8608929f08013cb30fc4c8daefd24ea57
1-Hee <onehee9710@gmail.com>
2023-02-11 10:31:09 +0900

    feat: 팔로워 팔로잉 기능 개발 중...

--- fe/src/pages/FollowerViewPage/UserInfoItem/UserInfoItem.js
+++ fe/src/pages/FollowerViewPage/UserInfoItem/UserInfoItem.js
@@ -3,65 +3,60 @@ import { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 
-
 /* import img */
 import userImg from "../../../assets/DummyData/dummy-sample.jpg";
 
 /* import component */
 
 /* import module */
-import { getFollowingList, getOtherFollowingList, cancelFollowing, doFollowing} from "../../../api/FollowAPI"
-
+import { getFollowingList, getOtherFollowingList, cancelFollowing, doFollowing } from "../../../api/FollowAPI";
 
 /* import css */
 import "./UserInfoItem.css";
 
-const UserInfoItem = ({ userInfo, isFollow }) => {
+const UserInfoItem = ({ userInfo, isMine }) => {
   const user = useSelector((state) => state.user.userData);
 
-  const [toggleFactor, setToggleFactor] = useState(isFollow);
-  const [followBtnStyle, setFollowBtnStyle] = useState("follow-btn");
+  const [toggleFactor, setToggleFactor] = useState(false);
   const [followText, setFollowText] = useState("팔로우");
-  const [myFollowList, setMyFollowList] = useState(false);
 
-  useEffect(() => {
-  }, [])
-  
-  let followingBtn = <div 
-  className={toggleFactor?"followed-btn":"follow-btn"}
-  onClick={(e) => {
-    toggleFunction();
-  }}
->
-  <p>{toggleFactor?"팔로잉":"팔로우"}</p>
-</div>
-
+  let followingBtn = (
+    <div
+      className={toggleFactor ? "followed-btn" : "follow-btn"}
+      onClick={(e) => {
+        toggleFunction();
+      }}
+    >
+      <p>{followText}</p>
+    </div>
+  );
 
   const toggleFunction = () => {
     // 팔로잉 관련 로직
     // true -> 팔로잉, 팔로우 한 상태
     // false -> 팔로우, 팔로우 안한 상태
-    if (toggleFactor) { 
-      setFollowBtnStyle("followed-btn");
+    if (toggleFactor) {
       setFollowText("팔로잉");
-      let data = cancelFollowing(userInfo.id);
-      data.then((res) => {
-        if (res) alert("팔로잉 취소 성공");
-      });
+      // let data = cancelFollowing(userInfo.id);
+      // data.then((res) => {
+      //   if (res) alert("팔로잉 취소 성공");
+      // });
     } else {
-      setFollowBtnStyle("follow-btn");
       setFollowText("팔로우");
-      let data = doFollowing(userInfo.id);
-      data.then((res) => {
-        if (res) alert("팔로잉 성공");
-      });      
+      // let data = doFollowing(userInfo.id);
+      // data.then((res) => {
+      //   if (res) alert("팔로잉 성공");
+      // });
     }
     setToggleFactor(!toggleFactor);
   };
   return (
     // userInfo.profileImage.saveName
     <div className="user-info-item" key={userInfo.id}>
-      <div className="user-img" style={{ backgroundImage: `url(${`https://i8b210.p.ssafy.io/api/file/${userInfo.profileImage.saveName}`})` }}></div>
+      <div
+        className="user-img"
+        style={{ backgroundImage: `url(${`https://i8b210.p.ssafy.io/api/file/${userInfo.profileImage.saveName}`})` }}
+      ></div>
       <div className="follower-info-div">
         <div className="user-nickname">
           <p>{userInfo.nickname}</p>
@@ -82,10 +77,9 @@ UserInfoItem.defaultProps = {
     introduction: "",
     nickname: "",
     profileImage: {
-      saveName:"",
-    }
+      saveName: "",
+    },
   },
-}
-
+};
 
 export default UserInfoItem;
