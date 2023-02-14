2808b30b632e5eb89f957f72c28249d91a22206b
김지환 <tykimdream@naver.com>
2023-02-14 13:50:40 +0900

    style: 회원가입 완료 페이지 구성 및 css 추가

--- fe/src/components/TextLogoComponent.js
+++ fe/src/components/TextLogoComponent.js
@@ -5,7 +5,7 @@ function TextLogoComponent() {
   return (
     <div className={styles.headertextframe}>
         <h3 className={styles.headertext}>당신이 마음이 꽃피는 곳</h3>
-        <h3 className={styles.headertext}>Flos</h3>
+        <h3 className={styles.subText}>Flos</h3>
         <br></br>
     </div>
   )
