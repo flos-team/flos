f5ed49d4f1f7ac51625e1e3ca3136cddfac62166
joykst96 <joykst96@gmail.com>
2023-02-16 13:27:08 +0900

    fix: 스웨거 수정

--- be/src/main/java/com/onehee/flos/controller/NotificationController.java
+++ be/src/main/java/com/onehee/flos/controller/NotificationController.java
@@ -3,11 +3,14 @@ package com.onehee.flos.controller;
 import com.onehee.flos.model.dto.request.NotificationCheckRequestDTO;
 import com.onehee.flos.model.dto.response.NotificationResponseDTO;
 import com.onehee.flos.model.service.NotificationService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "알람 API", description = "알람수신/확인 API")
 @RestController
 @RequestMapping("/notification")
 @RequiredArgsConstructor
@@ -15,6 +18,8 @@ public class NotificationController {
 
     private final NotificationService notificationService;
 
+    @Tag(name = "알람 API")
+    @Operation(summary = "알람 조회 메서드", description = "요청자의 알람 리스트를 반환합니다.")
     @GetMapping
     public ResponseEntity<?> getNotification() {
         NotificationResponseDTO notificationResponseDTO = notificationService.getNotification();
@@ -22,6 +27,8 @@ public class NotificationController {
         return new ResponseEntity<NotificationResponseDTO>(notificationResponseDTO, status);
     }
 
+    @Tag(name = "알람 API")
+    @Operation(summary = "알람 확인 메서드", description = "요청자의 알람을 확인 처리합니다.")
     @DeleteMapping("/{id}")
     public ResponseEntity<?> checkNotification(@PathVariable("id") Long id) {
         NotificationResponseDTO notificationResponseDTO = notificationService.checkNotification(new NotificationCheckRequestDTO(id));
--- be/src/main/java/com/onehee/flos/controller/SentimentController.java
+++ be/src/main/java/com/onehee/flos/controller/SentimentController.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.onehee.flos.model.dto.request.SentimentRequestDTO;
 import com.onehee.flos.model.dto.response.SentimentResponseDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Value;
@@ -18,6 +20,7 @@ import org.springframework.web.client.RestTemplate;
 
 import java.util.Map;
 
+@Tag(name = "감정분석 API", description = "감정분석 기능을 제공하는 API")
 @RestController
 @RequestMapping("/sentiment")
 @RequiredArgsConstructor
@@ -36,6 +39,8 @@ public class SentimentController {
     private final ObjectMapper objectMapper;
 
 
+    @Tag(name = "감정분석 API")
+    @Operation(summary = "감정분석 메서드", description = "게시글의 본문으로 추정한 감정추정치를 반환합니다.")
     @PostMapping
     public ResponseEntity<?> getEmotionalData(@RequestBody SentimentRequestDTO sentimentRequestDTO) throws JsonProcessingException {
 
